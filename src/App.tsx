0bd4a4e0b0af286d9b14239a432dd1414bd2d797
lim <limij206@gmail.com>
2026-01-20 19:08:50 +0900

    start new project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+function App() {
+  return (
+    <div className="p-10">
+      {/* この文字が「大きく」「太く」「青く」なっていれば成功です */}
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind v4 is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
